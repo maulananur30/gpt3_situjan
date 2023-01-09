7dd8053db17a776fd4f80e9db550dafdce825a67
maulana nur <maulananur30@gmail.com>
2023-01-10 00:19:30 +0700

    first commit

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatGPT3.css';
+
+const WhatGPT3 = () => {
+  return (
+    <div className="gpt3__whatgpt3 section__margin">
+      
+    </div>
+  )
+}
+
+export default WhatGPT3
